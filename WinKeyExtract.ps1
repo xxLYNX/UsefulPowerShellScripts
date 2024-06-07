3e029577d1c1868149a7cbd1ddace19d8be62667
Cullen Kelley <98487340+xxLYNX@users.noreply.github.com>
2024-06-06 21:21:44 -0400

    Added script WinKeyExtract
    Script does exactly what it says on the tin it pulls out the Windows product key from Windows 7, 10, and probably (not tested) 11.

---
+++ WinKeyExtract.ps1
@@ -0,0 +1,36 @@
+﻿function Get-WindowsLicenseKey {
+    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    $valueName = "DigitalProductId"
+
+    try {
+        $data = Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction Stop
+        $key = $data.$valueName
+
+        $productId = $key[52..66]
+        $chars = "BCDFGHJKMPQRTVWXY2346789"
+        $licenseKey = ""
+
+        for ($i = 24; $i -ge 0; $i--) {
+            $r = 0
+            for ($j = 14; $j -ge 0; $j--) {
+                $r = ($r * 256) -bxor $key[$j]
+                $key[$j] = [math]::Floor([double]($r / 24))
+                $r %= 24
+            }
+            $licenseKey = $chars[$r] + $licenseKey
+            if (($i % 5) -eq 0 -and $i -ne 0) {
+                $licenseKey = "-" + $licenseKey
+            }
+        }
+
+        return $licenseKey
+    } catch {
+        Write-Error "Unable to retrieve Windows license key. The operating system may not be activated or the registry key is inaccessible."
+    }
+}
+
+# Usage
+$licenseKey = Get-WindowsLicenseKey
+if ($licenseKey) {
+    Write-Host "Windows License Key: $licenseKey"
+}
